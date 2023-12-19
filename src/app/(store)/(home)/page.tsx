cf68b4533898aa30f9199d50ad065443a86cfe32
Rodolfo wsl <rndcsjj@gmail.com>
2023-12-19 16:33:24 -0300

    feat(metadata): aprendendo meta data entre as paginas

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,19 +1,23 @@
 import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
 
+export const metadata: Metadata = {
+  title: 'Home'
+}
+
 export default async function Home() {
   async function getFeaturedProducts(): Promise<Product[]> {
     const response = await api('/products/featured', {
-      cache: 'no-cache',
-      // next: {
-      //   revalidate: 60 * 60 //1 hour
-      // }
+      next: {
+        revalidate: 60 * 60 //1 hour
+      }
     })
-  
-    const products = await response.json()      
+
+    const products = await response.json()
     return products
   }
 
@@ -39,12 +43,12 @@ export default async function Home() {
         >
           <span className="text-sm truncate">{highLightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {highLightedProduct.price.toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL',
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0
-              })}
+            {highLightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0
+            })}
           </span>
         </div>
       </Link>
@@ -52,32 +56,32 @@ export default async function Home() {
       {otherProducts.map(product => {
         return (
           <Link
-          key={product.id}
-          href={`/product/${product.slug}`}
-          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-        >
-          <Image
-            src={product.image}
-            width={920}
-            height={920}
-            alt=""
-            quality={100}
-            className="group-hover:scale-105 transition-transform duration-500"
-          />
-          <div
-            className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
           >
-            <span className="text-sm truncate">{product.title}</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {product.price.toLocaleString('pt-BR', {
+            <Image
+              src={product.image}
+              width={920}
+              height={920}
+              alt=""
+              quality={100}
+              className="group-hover:scale-105 transition-transform duration-500"
+            />
+            <div
+              className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5"
+            >
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
                   style: 'currency',
                   currency: 'BRL',
                   minimumFractionDigits: 0,
                   maximumFractionDigits: 0
                 })}
-            </span>
-          </div>
-        </Link>
+              </span>
+            </div>
+          </Link>
         )
       })}
 
